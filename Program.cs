09e3f286cb40b35b5fe7c7767705b6508302b209
Vux Nguyen <vux@example.com>
2025-12-23 23:38:26 +0700

    đã up

--- Program.cs
+++ Program.cs
@@ -77,24 +77,25 @@
 
 
 ////////////////////
-Random dice = new Random();
+/// 
+// Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
+// int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 
 
-if (total > 14)
-{
-    Console.WriteLine("You win!");
-}
+// if (total > 14)
+// {
+//     Console.WriteLine("You win!");
+// }
 
-if (total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
-}
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
